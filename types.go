8f5aa03248e6ffb3311f51fe27a5d496b1a522e5
daisy1754 <d-part@hotmail.co.jp>
2019-11-13 14:05:56 -0800

    Add type definitions

---
+++ types.go
@@ -0,0 +1,62 @@
+package sleet
+
+type Client interface {
+	Authorize(request *AuthorizationRequest) (*AuthorizationResponse, error)
+	Capture(request *CaptureRequest) (*CaptureResponse, error)
+	Void(request *VoidRequest) (*VoidResponse, error)
+	Credit(request *CreditRequest) (*CreditResponse, error)
+}
+
+type Amount struct {
+	Amount   int64
+	Currency string
+}
+
+type CreditCard struct {
+	FirstName       string
+	LastName        string
+	Number          string
+	ExpirationMonth int
+	ExpirationYear  int
+	CVV             string
+}
+
+type AuthorizationRequest struct {
+	Amount     *Amount
+	CreditCard *CreditCard
+	Options    map[string]interface{}
+}
+
+type AuthorizationResponse struct {
+	Success              bool
+	TransactionReference string
+	AvsResult            string
+	CvvResult            string
+	ErrorCode            string
+}
+
+type CaptureRequest struct {
+	Amount               *Amount
+	TransactionReference string
+}
+
+type CaptureResponse struct {
+	ErrorCode *string
+}
+
+type VoidRequset struct {
+	TransactionReference string
+}
+
+type VoidResponse struct {
+	ErrorCode *string
+}
+
+type CreditRequest struct {
+	Amount               *Amount
+	TransactionReference string
+}
+
+type CreditResponse struct {
+	ErrorCode *string
+}
