718e79915e8a8cf105e46f53e980d91a1533af85
chaseadams509 <chase@bolt.com>
2019-11-13 14:28:26 -0800

    Fix (#2)

--- gateways/cybersource/cybersource.go
+++ gateways/cybersource/cybersource.go
@@ -1,10 +1,23 @@
 package cybersource
 
 import (
+	"crypto/tls"
 	"net/http"
+	"time"
 )
 
-var cybersourceBaseURL = "https://api.stripe.com"
+var baseURL = "https://api.stripe.com"
+
+var defaultHttpClient = &http.Client{
+	Timeout: 60 * time.Second,
+
+	// Disable HTTP2 by default (see stripe-go library - https://github.com/stripe/stripe-go/blob/d1d103ec32297246e5b086c867f3c18a166bf8bd/stripe.go#L1050 )
+	Transport: &http.Transport{
+		TLSNextProto: make(map[string]func(string, *tls.Conn) http.RoundTripper),
+	},
+}
+
+
 
 type CybersourceClient struct{
 	apiKey string // TODO: Check if need apiKey or some other auth
@@ -12,8 +25,8 @@ type CybersourceClient struct{
 	httpClient *http.Client
 }
 
-func NewCybersourceClient(apiKey string) *StripeClient {
-	return &StripeClient{
+func NewCybersourceClient(apiKey string) *CybersourceClient {
+	return &CybersourceClient{
 		apiKey:     apiKey,
 		httpClient: defaultHttpClient,
 	}
