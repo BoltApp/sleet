a23f504e9fa5f6c203ef192a3a2a7ae303f3311a
nirajjayantbolt <41930082+nirajjayantbolt@users.noreply.github.com>
2019-11-14 10:57:17 -0800

    Fixing authorize for Cybersource (#21)

--- gateways/cybersource/cypbersource_test.go
+++ gateways/cybersource/cypbersource_test.go
@@ -3,6 +3,7 @@ package cybersource
 import (
 	"testing"
 
+	"github.com/AlekSi/pointer"
 	"github.com/BoltApp/sleet"
 )
 
@@ -12,6 +13,8 @@ func Test(t *testing.T) {
 
 func TestAuthorize(t *testing.T) {
 	client := NewClient("bolt", "9b473fca-d9dc-4daf-baae-121e20af43ce", "2Ji1F/9mIYCJtdc2Enr5WvD8VBZ6sb0YS14asKinwQo=") // don't care if it leaks
+	options := make(map[string]interface{})
+	options["email"] = "test@bolt.com"
 	client.Authorize(&sleet.AuthorizationRequest{
 		Amount: &sleet.Amount{
 			Amount:   100,
@@ -25,5 +28,14 @@ func TestAuthorize(t *testing.T) {
 			ExpirationYear:  2024,
 			CVV:             "000",
 		},
+		BillingAddress: &sleet.BillingAddress{
+			StreetAddress1: pointer.ToString("77 Geary St"),
+			StreetAddress2: pointer.ToString("Floor 4"),
+			Locality:       pointer.ToString("San Francisco"),
+			RegionCode:     pointer.ToString("CA"),
+			PostalCode:     pointer.ToString("94108"),
+			CountryCode:    pointer.ToString("US"),
+		},
+		Options: options,
 	})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/BoltApp/sleet
 
 go 1.13
 
-require github.com/pkg/errors v0.8.1
+require (
+	github.com/AlekSi/pointer v1.1.0
+	github.com/pkg/errors v0.8.1
+)
