d6cd45b2c7914b2b393298b887a9e37300460438
ryan-timothy-albert <Ryan.Albert@Bolt.com>
2019-11-14 15:02:24 -0500

    Authorize beginning

--- gateways/adyen/request_builder.go
+++ gateways/adyen/request_builder.go
@@ -1,1 +1,21 @@
 package adyen
+
+import (
+	"github.com/BoltApp/sleet"
+	"strconv"
+)
+
+func buildAuthRequest(authRequest *sleet.AuthorizationRequest) (*AuthRequest, error) {
+	request := &AuthRequest{
+		Amount: authRequest.Amount,
+		PaymentMethod: &CreditCard{
+			    Type: "scheme",
+				ExpiryYear:  strconv.Itoa(authRequest.CreditCard.ExpirationYear),
+				ExpiryMonth: strconv.Itoa(authRequest.CreditCard.ExpirationMonth),
+				Number:   authRequest.CreditCard.Number,
+				CVC:      authRequest.CreditCard.CVV,
+				HolderName: authRequest.CreditCard.FirstName + " " + authRequest.CreditCard.LastName
+		},
+	}
+	return request, nil
+}
--- gateways/adyen/types.go
+++ gateways/adyen/types.go
@@ -1,1 +1,17 @@
 package adyen
+
+import "github.com/BoltApp/sleet"
+
+type AuthRequest struct {
+	Amount     *sleet.Amount `json:"amount"`
+	PaymentMethod *CreditCard `json:"paymentMethod"`
+}
+
+type CreditCard struct {
+	Type string `json:"type"`
+	Number string `json:"number"`
+	ExpiryMonth string `json:"expiryMonth"`
+	ExpiryYear string `json:"expiryYear"`
+	CVC        string  `json:"cvc"`
+	HolderName string  `json:"holderName"`
+}
