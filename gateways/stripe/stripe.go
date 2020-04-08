354d0d8e0a3fc69fa94b4c3b3a18c24ec1f2f266
Niraj Jayant <niraj@bolt.com>
2020-04-08 16:20:23 -0700

    [Stripe] - clean up all of this code - use library

--- gateways/stripe/stripe.go
+++ gateways/stripe/stripe.go
@@ -2,21 +2,16 @@ package stripe
 
 import (
 	"crypto/tls"
-	"encoding/json"
-	"fmt"
-	"github.com/BoltApp/sleet/common"
-	"io/ioutil"
 	"net/http"
-	net_url "net/url"
-	"strconv"
-	"strings"
 	"time"
 
-	"github.com/BoltApp/sleet"
-	"github.com/go-playground/form"
-)
+	"github.com/BoltApp/sleet/common"
+	"github.com/stripe/stripe-go"
 
-var baseURL = "https://api.stripe.com"
+	"github.com/BoltApp/sleet"
+	"github.com/stripe/stripe-go/charge"
+	"github.com/stripe/stripe-go/refund"
+)
 
 type StripeClient struct {
 	apiKey     string
@@ -37,6 +32,8 @@ func NewClient(apiKey string) *StripeClient {
 }
 
 func NewWithHTTPClient(apiKey string, httpClient *http.Client) *StripeClient {
+	// set the Stripe global key for requests
+	stripe.Key = apiKey
 	return &StripeClient{
 		apiKey:     apiKey,
 		httpClient: httpClient,
@@ -44,152 +41,43 @@ func NewWithHTTPClient(apiKey string, httpClient *http.Client) *StripeClient {
 }
 
 func (client *StripeClient) Authorize(request *sleet.AuthorizationRequest) (*sleet.AuthorizationResponse, error) {
-	tokenRequest, err := buildAuthRequest(request)
+	chargeClient := charge.Client{B: stripe.GetBackend(stripe.APIBackend), Key: client.apiKey}
+	charge, err := chargeClient.New(buildChargeParams(request))
 	if err != nil {
-		return nil, err
+		return &sleet.AuthorizationResponse{Success: false, TransactionReference: "", AvsResult: sleet.AVSResponseUnknown, CvvResult: sleet.CVVResponseUnknown, ErrorCode: err.Error()}, nil
 	}
-	encoder := form.NewEncoder()
-	form, err := encoder.Encode(tokenRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	code, resp, err := client.sendRequest("v1/tokens", form)
-	if err != nil {
-		return nil, err
-	}
-	var tokenResponse TokenResponse
-	if code != 200 {
-		return &sleet.AuthorizationResponse{Success: false, TransactionReference: "", AvsResult: sleet.AVSResponseUnknown, CvvResult: sleet.CVVResponseUnknown, ErrorCode: strconv.Itoa(code)}, nil
-	}
-	if err := json.Unmarshal(resp, &tokenResponse); err != nil {
-		return nil, err
-	}
-	fmt.Printf("response %s\n", tokenResponse.ID) // debug
-	chargeRequest, err := buildChargeRequest(request, tokenResponse.ID)
-	if err != nil {
-		return nil, err
-	}
-	form, err = encoder.Encode(chargeRequest)
-	if err != nil {
-		return nil, err
-	}
-	code, resp, err = client.sendRequest("v1/charges", form)
-	if err != nil {
-		return nil, err
-	}
-	if code != 200 {
-		return &sleet.AuthorizationResponse{Success: false, TransactionReference: "", AvsResult: sleet.AVSResponseUnknown, CvvResult: sleet.CVVResponseUnknown, ErrorCode: strconv.Itoa(code)}, nil
-	}
-	var chargeResponse ChargeResponse
-	if err := json.Unmarshal(resp, &chargeResponse); err != nil {
-		return nil, err
-	}
-
 	return &sleet.AuthorizationResponse{
 		Success:              true,
-		TransactionReference: chargeResponse.ID,
+		TransactionReference: charge.ID,
 		AvsResult:            sleet.AVSresponseZipMatchAddressMatch, // TODO: Add translator
 		CvvResult:            sleet.CVVResponseMatch,                // TODO: Add translator
-		AvsResultRaw:         *tokenResponse.Card.AddressZipCheck,
-		CvvResultRaw:         tokenResponse.Card.CVCCheck,
-		ErrorCode:            strconv.Itoa(code)}, nil
+		AvsResultRaw:         string(charge.Source.Card.AddressLine1Check),
+		CvvResultRaw:         string(charge.Source.Card.CVCCheck)}, nil
 }
 
 func (client *StripeClient) Capture(request *sleet.CaptureRequest) (*sleet.CaptureResponse, error) {
-	capturePath := fmt.Sprintf("v1/charges/%s/capture", request.TransactionReference)
-	captureRequest, err := buildCaptureRequest(request)
+	chargeClient := charge.Client{B: stripe.GetBackend(stripe.APIBackend), Key: client.apiKey}
+	capture, err := chargeClient.Capture(request.TransactionReference, buildCaptureParams(request))
 	if err != nil {
-		return nil, err
+		return &sleet.CaptureResponse{Success: false, ErrorCode: common.SPtr(err.Error())}, nil
 	}
-	encoder := form.NewEncoder()
-	form, err := encoder.Encode(captureRequest)
-	if err != nil {
-		return nil, err
-	}
-	code, resp, err := client.sendRequest(capturePath, form)
-	if err != nil {
-		return nil, err
-	}
-	convertedCode := strconv.Itoa(code)
-	fmt.Printf("response capture %s\n", string(resp)) // debug
-	return &sleet.CaptureResponse{Success: true, ErrorCode: &convertedCode}, nil
+	return &sleet.CaptureResponse{Success: true, TransactionReference: capture.ID}, nil
 }
 
 func (client *StripeClient) Refund(request *sleet.RefundRequest) (*sleet.RefundResponse, error) {
-	refundRequest, err := buildRefundRequest(request)
+	refundClient := refund.Client{B: stripe.GetBackend(stripe.APIBackend), Key: client.apiKey}
+	refund, err := refundClient.New(buildRefundParams(request))
 	if err != nil {
-		return nil, err
+		return &sleet.RefundResponse{Success: false, ErrorCode: common.SPtr(err.Error())}, nil
 	}
-	encoder := form.NewEncoder()
-	form, err := encoder.Encode(refundRequest)
-	if err != nil {
-		return nil, err
-	}
-	code, resp, err := client.sendRequest("v1/refunds", form)
-	if err != nil {
-		return nil, err
-	}
-	convertedCode := strconv.Itoa(code)
-	fmt.Printf("response refund %s\n", string(resp)) // debug
-	return &sleet.RefundResponse{Success: true, ErrorCode: &convertedCode}, nil
+	return &sleet.RefundResponse{Success: true, TransactionReference: refund.ID}, nil
 }
 
 func (client *StripeClient) Void(request *sleet.VoidRequest) (*sleet.VoidResponse, error) {
-	voidRequest, err := buildVoidRequest(request)
+	voidClient := refund.Client{B: stripe.GetBackend(stripe.APIBackend), Key: client.apiKey}
+	void, err := voidClient.New(buildVoidParams(request))
 	if err != nil {
-		return nil, err
+		return &sleet.VoidResponse{Success: false, ErrorCode: common.SPtr(err.Error())}, nil
 	}
-	encoder := form.NewEncoder()
-	form, err := encoder.Encode(voidRequest)
-	if err != nil {
-		return nil, err
-	}
-	code, resp, err := client.sendRequest("v1/refunds", form)
-	if err != nil {
-		return nil, err
-	}
-	convertedCode := strconv.Itoa(code)
-	fmt.Printf("response void %s\n", string(resp)) // debug
-	return &sleet.VoidResponse{Success: true, ErrorCode: &convertedCode}, nil
-}
-
-func (client *StripeClient) sendRequest(path string, data net_url.Values) (int, []byte, error) {
-	req, err := client.buildPOSTRequest(path, data)
-	if err != nil {
-		return -1, nil, err
-	}
-	req.Header.Add("User-Agent", common.UserAgent())
-	resp, err := client.httpClient.Do(req)
-	if err != nil {
-		return -1, nil, err
-	}
-	defer func() {
-		err := resp.Body.Close()
-		if err != nil {
-			// TODO log
-		}
-	}()
-
-	fmt.Printf("status %s\n", resp.Status) // debug
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf("body %s\n", body) // debug
-	return resp.StatusCode, body, err
-}
-
-func (client *StripeClient) buildPOSTRequest(path string, data net_url.Values) (*http.Request, error) {
-	url := baseURL + "/" + path
-
-	fmt.Printf("data %s\n", data.Encode()) // debug
-	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(data.Encode()))
-	if err != nil {
-		return nil, err
-	}
-
-	authorization := "Bearer " + client.apiKey
-	req.Header.Add("Authorization", authorization)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("User-Agent", "sleet")
-
-	return req, nil
+	return &sleet.VoidResponse{Success: true, TransactionReference: void.ID}, nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/go-playground/form v3.1.4+incompatible
 	github.com/jarcoal/httpmock v1.0.4
 	github.com/stretchr/testify v1.4.0
+	github.com/stripe/stripe-go v70.11.0+incompatible
+	github.com/zhutik/adyen-api-go v0.0.0-20200322135654-771fae870bd2 // indirect
 	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
