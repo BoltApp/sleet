ee7b04446c8f926ebaacbffd7d0de8530e484ade
nirajjayantbolt <41930082+nirajjayantbolt@users.noreply.github.com>
2020-04-16 20:30:59 -0700

    [Adyen] - take reference from ClientTransactionReference (#70)
    * [Adyen] - take reference from ClientTransactionReference
    
    * fixing tests

--- integration-tests/adyen_test.go
+++ integration-tests/adyen_test.go
@@ -4,21 +4,14 @@ import (
 	"github.com/BoltApp/sleet"
 	"github.com/BoltApp/sleet/gateways/adyen"
 	sleet_testing "github.com/BoltApp/sleet/testing"
-	"github.com/Pallinder/go-randomdata"
-	"testing"
 	adyen_go "github.com/zhutik/adyen-api-go"
+	"testing"
 )
 
 func TestAdyenAuthorize(t *testing.T) {
 	client := adyen.NewClient(adyen_go.Testing, getEnv("ADYEN_USERNAME"), getEnv("ADYEN_ACCOUNT"), getEnv("ADYEN_PASSWORD"))
 	authRequest1 := sleet_testing.BaseAuthorizationRequest()
 	authRequest2 := sleet_testing.BaseAuthorizationRequest()
-	options := make(map[string]interface{})
-	options2 := make(map[string]interface{})
-	options["reference"] = randomdata.Letters(10) // so we don't collide with adyen
-	options2["reference"] = randomdata.Letters(10)
-	authRequest1.Options = options
-	authRequest2.Options = options2
 	auth, _ := client.Authorize(authRequest1)
 	client.Capture(&sleet.CaptureRequest{Amount: &authRequest1.Amount, TransactionReference: auth.TransactionReference})
 	client.Refund(&sleet.RefundRequest{Amount: &authRequest1.Amount, TransactionReference: auth.TransactionReference})
