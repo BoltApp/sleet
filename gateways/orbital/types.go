93b37c355e529ac2c685abbc2a2129c4311085ce
steams <radclifferobs@gmail.com>
2020-08-24 08:03:16 -0500

    Update types

--- gateways/orbital/types.go
+++ gateways/orbital/types.go
@@ -5,10 +5,9 @@ import "encoding/xml"
 type RequestType string
 
 const (
-	RequestTypeAuth    = "NewOrder"
-	RequestTypeCapture = "MarkForCapture"
-	RequestTypeVoid    = "Reversal"
-	RequestTypeRefund  = "Reversal"
+	RequestTypeNewOrder = "NewOrder"
+	RequestTypeCapture  = "MarkForCapture"
+	RequestTypeVoid     = "Reversal"
 )
 
 type MessageType string
@@ -19,6 +18,7 @@ const (
 	MessageTypeCapture        MessageType = "FC"
 	MessageTypeRefund         MessageType = "R"
 )
+
 const TerminalIDStratus string = "001"
 
 type BIN string
@@ -38,12 +38,20 @@ const (
 	IndustryTypeRecurring   IndustryType = "RC"
 )
 
-type CardSecValInd int
+type CardSecValInd int // Card Security Presence Indicator for for a Visa or Discover transaction
 
 const (
-	CardSecPresent      CardSecValInd = 1
-	CardSecIllegible    CardSecValInd = 2
-	CardSecNotAvailable CardSecValInd = 9
+	CardSecPresent      CardSecValInd = 1 // Value is Present
+	CardSecIllegible    CardSecValInd = 2 // Value on card but illegiable
+	CardSecNotAvailable CardSecValInd = 9 // Cardholder states data not available
+)
+
+type ApprovalStatus int
+
+const (
+	ApprovalStatusDeclined ApprovalStatus = 0
+	ApprovalStatusApproved ApprovalStatus = 1
+	ApprovalStatusError    ApprovalStatus = 2
 )
 
 type CVVResponseCode string
@@ -81,7 +89,7 @@ const (
 
 type CurrencyExponent string
 
-const CurrencyExponentDefault = "2"
+const CurrencyExponentDefault CurrencyExponent = "2"
 
 type Request struct {
 	XMLName xml.Name `xml:"Request"`
@@ -89,37 +97,37 @@ type Request struct {
 }
 
 type Response struct {
-	XMLName xml.Name `xml:"Response"`
-	Body    ResponseBody
+	XMLName xml.Name     `xml:"Response"`
+	Body    ResponseBody `xml:",any"`
 }
 
 type RequestBody struct {
 	XMLName                   xml.Name
-	OrbitalConnectionUsername string        `xml:"OrbitalConnectionUsername"`
-	OrbitalConnectionPassword string        `xml:"OrbitalConnectionPassword"`
-	BIN                       BIN           `xml:"BIN"`
-	TerminalID                string        `xml:"TerminalID"` // usually 001, for PNS can be 001 - 999 but usually 001
-	IndustryType              IndustryType  `xml:"IndustryType,omitempty"`
-	MessageType               MessageType   `xml:"MessageType,omitempty"`
-	MerchantID                int           `xml:"MerchantID,omitempty"`
-	AccountNum                string        `xml:"AccountNum,omitempty"`
-	Exp                       string        `xml:"Exp,omitempty"` //Format: MMYY or YYYYMM
-	CurrencyCode              CurrencyCode  `xml:"CurrencyCode,omitempty"`
-	CurrencyExponent          string        `xml:"CurrencyExponent,omitempty"`
-	CardSecValInd             CardSecValInd `xml:"CardSecValInd,omitempty"`
-	CardSecVal                string        `xml:"CardSecVal,omitempty"`
-	OrderID                   string        `xml:"OrderID,omitempty"`     // generated id, max 22 chars
-	Amount                    int64         `xml:"Amount,omitempty"`      //int with the last 2 digits being implied decimals ie 100.25 is sent as 10025, 90 is sent as 9000
-	AdjustedAmt               int64         `xml:"AdjustedAmt,omitempty"` //int with the last 2 digits being implied decimals ie 100.25 is sent as 10025, 90 is sent as 9000
-	TxRefNum                  string        `xml:"TxRefNum,omitempty"`
-	AVSzip                    string        `xml:"AVSzip,omitempty"`
-	AVSaddress1               string        `xml:"AVSaddress1,omitempty"`
-	AVSaddress2               string        `xml:"AVSaddress2,omitempty"`
-	AVSstate                  string        `xml:"AVSstate,omitempty"`
-	AVScity                   string        `xml:"AVScity,omitempty"`
-	AVSname                   string        `xml:"AVSname,omitempty"`
-	AVScountryCode            string        `xml:"AVScountryCode,omitempty"`
-	AVSphoneNum               string        `xml:"AVSphoneNum,omitempty"`
+	OrbitalConnectionUsername string           `xml:"OrbitalConnectionUsername"`
+	OrbitalConnectionPassword string           `xml:"OrbitalConnectionPassword"`
+	BIN                       BIN              `xml:"BIN"`
+	TerminalID                string           `xml:"TerminalID"` // usually 001, for PNS can be 001 - 999 but usually 001
+	IndustryType              IndustryType     `xml:"IndustryType,omitempty"`
+	MessageType               MessageType      `xml:"MessageType,omitempty"`
+	MerchantID                int              `xml:"MerchantID,omitempty"`
+	AccountNum                string           `xml:"AccountNum,omitempty"`
+	Exp                       string           `xml:"Exp,omitempty"` //Format: MMYY or YYYYMM
+	CurrencyCode              CurrencyCode     `xml:"CurrencyCode,omitempty"`
+	CurrencyExponent          CurrencyExponent `xml:"CurrencyExponent,omitempty"`
+	CardSecValInd             CardSecValInd    `xml:"CardSecValInd,omitempty"`
+	CardSecVal                string           `xml:"CardSecVal,omitempty"`
+	OrderID                   string           `xml:"OrderID,omitempty"`     // generated id, max 22 chars
+	Amount                    int64            `xml:"Amount,omitempty"`      //int with the last 2 digits being implied decimals ie 100.25 is sent as 10025, 90 is sent as 9000
+	AdjustedAmt               int64            `xml:"AdjustedAmt,omitempty"` //int with the last 2 digits being implied decimals ie 100.25 is sent as 10025, 90 is sent as 9000
+	TxRefNum                  string           `xml:"TxRefNum,omitempty"`
+	AVSzip                    string           `xml:"AVSzip,omitempty"`
+	AVSaddress1               string           `xml:"AVSaddress1,omitempty"`
+	AVSaddress2               *string          `xml:"AVSaddress2,omitempty"`
+	AVSstate                  string           `xml:"AVSstate,omitempty"`
+	AVScity                   string           `xml:"AVScity,omitempty"`
+	AVSname                   string           `xml:"AVSname,omitempty"`
+	AVScountryCode            string           `xml:"AVScountryCode,omitempty"`
+	AVSphoneNum               string           `xml:"AVSphoneNum,omitempty"`
 }
 
 type ResponseBody struct {
@@ -132,9 +140,11 @@ type ResponseBody struct {
 	OrderID        string          `xml:"OrderID"`
 	TxRefNum       int             `xml:"TxRefNum"`
 	TxRefIdx       int             `xml:"TxRefIdx"`
+	RespCode       string          `xml:"RespCode"`
+	StatusMsg      string          `xml:"StatusMsg"`
 	ProcStatus     int             `xml:"ProcStatus"`
 	AVSRespCode    AVSResponseCode `xml:"AVSRespCode"`
 	CVV2RespCode   CVVResponseCode `xml:"CVV2RespCode"`
-	ApprovalStatus int             `xml:"ApprovalStatus"`
+	ApprovalStatus ApprovalStatus  `xml:"ApprovalStatus"`
 	RedeemedAmount int             `xml:"RedeemedAmount"`
 }
