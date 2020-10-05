bc607373ab17f365adb42b78dfd388e000d5c23a
Daniel Book <36259973+dbook13@users.noreply.github.com>
2020-10-05 14:19:08 -0600

    [adyen] use Adyen specific names for credit card brands (#130)

--- credit_card_network.go
+++ credit_card_network.go
@@ -11,17 +11,3 @@ const (
 	CreditCardNetworkJcb
 	CreditCardNetworkUnionpay
 )
-
-var creditCardNetworkToString = map[CreditCardNetwork]string{
-	CreditCardNetworkUnknown:    "Unknown",
-	CreditCardNetworkVisa:       "Visa",
-	CreditCardNetworkMastercard: "Mastercard",
-	CreditCardNetworkAmex:       "Amex",
-	CreditCardNetworkDiscover:   "Discover",
-	CreditCardNetworkJcb:        "Jcb",
-	CreditCardNetworkUnionpay:   "Unionpay",
-}
-
-func (code CreditCardNetwork) String() string {
-	return creditCardNetworkToString[code]
-}
