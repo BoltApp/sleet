1993db6d04495b1a6da49b5fd00bb0df88582869
Kazuki <kazuki@bolt.com>
2020-11-19 09:52:13 -0800

    Improve golint (#140)

--- credit_card_network.go
+++ credit_card_network.go
@@ -1,13 +1,21 @@
 package sleet
 
+// CreditCardNetwork card network (eg visa)
 type CreditCardNetwork int
 
 const (
+	// CreditCardNetworkUnknown unknown network
 	CreditCardNetworkUnknown CreditCardNetwork = iota
+	// CreditCardNetworkVisa visa
 	CreditCardNetworkVisa
+	// CreditCardNetworkMastercard mastercard
 	CreditCardNetworkMastercard
+	// CreditCardNetworkAmex amex
 	CreditCardNetworkAmex
+	// CreditCardNetworkDiscover discover
 	CreditCardNetworkDiscover
+	// CreditCardNetworkJcb JCBP
 	CreditCardNetworkJcb
+	// CreditCardNetworkUnionpay UnionPay
 	CreditCardNetworkUnionpay
 )
--- processing_initiator_type.go
+++ processing_initiator_type.go
@@ -1,11 +1,17 @@
 package sleet
 
+// ProcessingInitiatorType type of processing initiator
 type ProcessingInitiatorType string
 
 const (
+	// ProcessingInitiatorTypeInitialCardOnFile initial non-recurring payment
 	ProcessingInitiatorTypeInitialCardOnFile         ProcessingInitiatorType = "initial_card_on_file"
+	// ProcessingInitiatorTypeInitialRecurring initiating recurring payment
 	ProcessingInitiatorTypeInitialRecurring          ProcessingInitiatorType = "initial_recurring"
+	// ProcessingInitiatorTypeStoredCardholderInitiated initiated by cardholder using stored card
 	ProcessingInitiatorTypeStoredCardholderInitiated ProcessingInitiatorType = "stored_cardholder_initiated"
+	// ProcessingInitiatorTypeStoredMerchantInitiated initiated by merchant using stored card
 	ProcessingInitiatorTypeStoredMerchantInitiated   ProcessingInitiatorType = "stored_merchant_initiated"
+	// ProcessingInitiatorTypeFollowingRecurring recurring payment
 	ProcessingInitiatorTypeFollowingRecurring        ProcessingInitiatorType = "following_recurring"
 )
