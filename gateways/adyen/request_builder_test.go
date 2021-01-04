a866c7574836d7616b348b5266fba0cae6402e6a
nirajjayant <41930082+nirajjayant@users.noreply.github.com>
2021-01-04 17:25:04 -0600

    Adyen - dont put discount amount if its empty (#156)
    * Adyen - dont put discount amount if its empty
    
    * fixing test
    
    * really fixing test
    
    * fixing logic now
    
    * fixing tests

--- gateways/adyen/request_builder_test.go
+++ gateways/adyen/request_builder_test.go
@@ -16,6 +16,9 @@ func TestBuildAuthRequest(t *testing.T) {
 	base := sleet_testing.BaseAuthorizationRequest()
 	requestWithLevel3Data := sleet_testing.BaseAuthorizationRequest()
 	requestWithLevel3Data.Level3Data = sleet_testing.BaseLevel3Data()
+	requestWithLevel3ItemDiscount := sleet_testing.BaseAuthorizationRequest()
+	requestWithLevel3ItemDiscount.Level3Data = sleet_testing.BaseLevel3Data()
+	requestWithLevel3ItemDiscount.Level3Data.LineItems[0].ItemDiscountAmount.Amount = 100
 
 	cases := []struct {
 		label string
@@ -53,7 +56,7 @@ func TestBuildAuthRequest(t *testing.T) {
 			},
 		},
 		{
-			"Auth Request with level3 data",
+			"Auth Request with level3 data no Item Discount",
 			requestWithLevel3Data,
 			&checkout.PaymentRequest{
 				Amount: checkout.Amount{
@@ -91,7 +94,52 @@ func TestBuildAuthRequest(t *testing.T) {
 					"enhancedSchemeData.itemDetailLine1.quantity": "2",
 					"enhancedSchemeData.itemDetailLine1.unitPrice": "500",
 					"enhancedSchemeData.itemDetailLine1.totalAmount" : "1000",
-					"enhancedSchemeData.itemDetailLine1.discountAmount": "0",
+					"enhancedSchemeData.destinationCountryCode": "US",
+					"enhancedSchemeData.customerReference": "customer",
+					"enhancedSchemeData.destinationPostalCode": "94105",
+				},
+			},
+		},
+		{
+			"Auth Request with level3 data with Item Discount",
+			requestWithLevel3ItemDiscount,
+			&checkout.PaymentRequest{
+				Amount: checkout.Amount{
+					Currency: "USD",
+					Value:    100,
+				},
+				BillingAddress: &checkout.Address{
+					City:              *requestWithLevel3Data.BillingAddress.Locality,
+					Country:           *requestWithLevel3Data.BillingAddress.CountryCode,
+					PostalCode:        *requestWithLevel3Data.BillingAddress.PostalCode,
+					StateOrProvince:   *requestWithLevel3Data.BillingAddress.RegionCode,
+					Street:            *requestWithLevel3Data.BillingAddress.StreetAddress1,
+				},
+				MerchantAccount:        "merchant-account",
+				PaymentMethod: map[string]interface{}{
+					"number":      requestWithLevel3Data.CreditCard.Number,
+					"expiryMonth": strconv.Itoa(requestWithLevel3Data.CreditCard.ExpirationMonth),
+					"expiryYear":  strconv.Itoa(requestWithLevel3Data.CreditCard.ExpirationYear),
+					"holderName":  requestWithLevel3Data.CreditCard.FirstName + " " + requestWithLevel3Data.CreditCard.LastName,
+					"cvc": requestWithLevel3Data.CreditCard.CVV,
+					"type":        "scheme",
+				},
+				ShopperInteraction: "Ecommerce",
+				RecurringProcessingModel: "CardOnFile",
+				Reference: *requestWithLevel3ItemDiscount.ClientTransactionReference,
+				StorePaymentMethod: true,
+				AdditionalData: map[string]string{
+					"enhancedSchemeData.totalTaxAmount": "100",
+					"enhancedSchemeData.freightAmount": "300",
+					"enhancedSchemeData.dutyAmount": "400",
+					"enhancedSchemeData.itemDetailLine1.description": "pot",
+					"enhancedSchemeData.itemDetailLine1.commodityCode": "cmd",
+					"enhancedSchemeData.itemDetailLine1.productCode": "abc",
+					"enhancedSchemeData.itemDetailLine1.unitOfMeasure": "EA",
+					"enhancedSchemeData.itemDetailLine1.quantity": "2",
+					"enhancedSchemeData.itemDetailLine1.unitPrice": "500",
+					"enhancedSchemeData.itemDetailLine1.totalAmount" : "1000",
+					"enhancedSchemeData.itemDetailLine1.discountAmount": "100",
 					"enhancedSchemeData.destinationCountryCode": "US",
 					"enhancedSchemeData.customerReference": "customer",
 					"enhancedSchemeData.destinationPostalCode": "94105",
