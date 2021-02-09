0c9f3e009be908b78ef208e5a97cea3d2a4234f3
kenbolt <54861412+kenbolt@users.noreply.github.com>
2021-02-08 22:17:47 -0800

    [Initiator Flags] Adyen (#161)

--- gateways/adyen/request_builder.go
+++ gateways/adyen/request_builder.go
@@ -15,6 +15,34 @@ const (
 	maxProductCodeLength         = 12
 )
 
+const (
+	shopperInteractionEcommerce = "Ecommerce"
+	shopperInteractionContAuth  = "ContAuth"
+)
+
+const (
+	recurringProcessingModelCardOnFile = "CardOnFile"
+	recurringProcessingModelSubscription = "Subscription"
+	recurringProcessingModelUnscheduledCardOnFile = "UnscheduledCardOnFile"
+)
+
+// these maps are based on https://docs.adyen.com/online-payments/tokenization/create-and-use-tokens#set-parameters-to-flag-transactions
+var initiatorTypeToShopperInteraction = map[sleet.ProcessingInitiatorType]string{
+	sleet.ProcessingInitiatorTypeInitialCardOnFile:         shopperInteractionEcommerce,
+	sleet.ProcessingInitiatorTypeInitialRecurring:          shopperInteractionEcommerce,
+	sleet.ProcessingInitiatorTypeStoredCardholderInitiated: shopperInteractionContAuth,
+	sleet.ProcessingInitiatorTypeStoredMerchantInitiated:   shopperInteractionContAuth,
+	sleet.ProcessingInitiatorTypeFollowingRecurring:        shopperInteractionContAuth,
+}
+
+var initiatorTypeToRecurringProcessingModel = map[sleet.ProcessingInitiatorType]string{
+	sleet.ProcessingInitiatorTypeInitialCardOnFile:         recurringProcessingModelCardOnFile,
+	sleet.ProcessingInitiatorTypeInitialRecurring:          recurringProcessingModelSubscription,
+	sleet.ProcessingInitiatorTypeStoredCardholderInitiated: recurringProcessingModelCardOnFile,
+	sleet.ProcessingInitiatorTypeStoredMerchantInitiated:   recurringProcessingModelUnscheduledCardOnFile,
+	sleet.ProcessingInitiatorTypeFollowingRecurring:        recurringProcessingModelSubscription,
+}
+
 func buildAuthRequest(authRequest *sleet.AuthorizationRequest, merchantAccount string) *checkout.PaymentRequest {
 	request := &checkout.PaymentRequest{
 		Amount: checkout.Amount{
@@ -46,6 +74,16 @@ func buildAuthRequest(authRequest *sleet.AuthorizationRequest, merchantAccount s
 	}
 
 	addPaymentSpecificFields(authRequest, request)
+
+	// overwrites the flag transactions
+	if authRequest.ProcessingInitiator != nil {
+		if shopperInteraction, ok := initiatorTypeToShopperInteraction[*authRequest.ProcessingInitiator]; ok {
+			request.ShopperInteraction = shopperInteraction
+		}
+		if recurringProcessingModel, ok := initiatorTypeToRecurringProcessingModel[*authRequest.ProcessingInitiator]; ok {
+			request.RecurringProcessingModel = recurringProcessingModel
+		}
+	}
 
 	level3 := authRequest.Level3Data
 	if level3 != nil {
