4550dfa6fd2a84d2b30e26bfa907e81d579e81c0
rlcooper46 <robert.cooper@bolt.com>
2021-12-01 12:17:36 -0800

    authorize_net ->  l2l3 support (#191)
    * support l2l3 data authnet
    
    * add integration tests
    
    * fix encoding
    
    * fix unit test
    
    * PR comments
    
    * remove old code
    
    * fix build

--- testing/auth_request.go
+++ testing/auth_request.go
@@ -74,6 +74,62 @@ func BaseLevel3Data() *sleet.Level3Data {
 	}
 }
 
+func BaseLevel3DataMultipleItem() *sleet.Level3Data {
+	return &sleet.Level3Data{
+		CustomerReference: "customer",
+		TaxAmount: sleet.Amount{
+			Amount:   100,
+			Currency: "USD",
+		},
+		DiscountAmount: sleet.Amount{
+			Amount:   200,
+			Currency: "USD",
+		},
+		ShippingAmount: sleet.Amount{
+			Amount:   300,
+			Currency: "USD",
+		},
+		DutyAmount: sleet.Amount{
+			Amount:   400,
+			Currency: "USD",
+		},
+		DestinationPostalCode:  "94105",
+		DestinationCountryCode: "US",
+		LineItems: []sleet.LineItem{
+			{
+				Description: "pot",
+				ProductCode: "abc",
+				UnitPrice: sleet.Amount{
+					Amount:   500,
+					Currency: "USD",
+				},
+				Quantity: 2,
+				TotalAmount: sleet.Amount{
+					Amount:   1000,
+					Currency: "USD",
+				},
+				UnitOfMeasure: "count",
+				CommodityCode: "cmd",
+			},
+			{
+				Description: "vase",
+				ProductCode: "123",
+				UnitPrice: sleet.Amount{
+					Amount:   1000,
+					Currency: "USD",
+				},
+				Quantity: 5,
+				TotalAmount: sleet.Amount{
+					Amount:   2000,
+					Currency: "USD",
+				},
+				UnitOfMeasure: "count",
+				CommodityCode: "321",
+			},
+		},
+	}
+}
+
 func BaseCaptureRequest() *sleet.CaptureRequest {
 	clientRef := "222222"
 
