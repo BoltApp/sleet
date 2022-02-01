a0035c7037abd67ef666816d506a796feb093c3f
Corey Fogg <84813106+cfogg-bolt@users.noreply.github.com>
2022-02-01 12:34:51 -0800

    Checkout.com Integration (#200)
    Added checkout.com integration

---
+++ gateways/checkoutcom/checkoutcom.go
@@ -0,0 +1,178 @@
+package checkoutcom
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/BoltApp/sleet"
+	"github.com/BoltApp/sleet/common"
+	"github.com/checkout/checkout-sdk-go"
+	"github.com/checkout/checkout-sdk-go/payments"
+	"net/http"
+	"strconv"
+)
+
+// checkout.com documentation here: https://www.checkout.com/docs/four/payments/accept-payments, SDK here: https://github.com/checkout/checkout-sdk-go
+
+// checkoutomClient uses API-Key and custom http client to make http calls
+type CheckoutComClient struct {
+	apiKey     string
+	httpClient *http.Client
+}
+
+const AcceptedStatusCode = 202
+
+// NewClient creates a CheckoutComClient
+// Note: the environment is indicated by the apiKey. See "isSandbox" assignment in checkout.Create.
+func NewClient(apiKey string) *CheckoutComClient {
+	return NewWithHTTPClient(apiKey, common.DefaultHttpClient())
+}
+
+// NewWithHTTPClient uses a custom http client for requests
+func NewWithHTTPClient(apiKey string, httpClient *http.Client) *CheckoutComClient {
+	return &CheckoutComClient{
+		apiKey:     apiKey,
+		httpClient: httpClient,
+	}
+}
+
+func (client *CheckoutComClient) generateCheckoutDCClient() (*payments.Client, error) {
+	config, err := checkout.Create(client.apiKey, nil)
+	if err != nil {
+		return nil, err
+	}
+	config.HTTPClient = client.httpClient
+
+	return payments.NewClient(*config), nil
+}
+
+// Authorize a transaction for specified amount
+func (client *CheckoutComClient) Authorize(request *sleet.AuthorizationRequest) (*sleet.AuthorizationResponse, error) {
+	checkoutComClient, err := client.generateCheckoutDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	input, err := buildChargeParams(request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := checkoutComClient.Request(input, nil)
+
+	if err != nil {
+		return &sleet.AuthorizationResponse{Success: false, TransactionReference: "", AvsResult: sleet.AVSResponseUnknown, CvvResult: sleet.CVVResponseUnknown, ErrorCode: err.Error()}, err
+	}
+
+	out, _ := json.Marshal(response.StatusResponse.ResponseBody)
+	fmt.Printf(string(out))
+
+	if *response.Processed.Approved {
+		return &sleet.AuthorizationResponse{
+			Success:              true,
+			TransactionReference: response.Processed.ID,
+			AvsResult:            sleet.AVSresponseZipMatchAddressMatch, // TODO: Use translateAvs(AVSResponseCode(response.Processed.Source.AVSCheck)) to enable avs code handling
+			CvvResult:            sleet.CVVResponseMatch,                // TODO: use translateCvv(CVVResponseCode(response.Processed.Source.CVVCheck)) to enable cvv code handling
+			AvsResultRaw:         response.Processed.Source.AVSCheck,
+			CvvResultRaw:         response.Processed.Source.CVVCheck,
+			Response:             response.Processed.ResponseCode,
+		}, nil
+	} else {
+		return &sleet.AuthorizationResponse{
+			Success:              false,
+			TransactionReference: "",
+			AvsResult:            sleet.AVSResponseUnknown,
+			CvvResult:            sleet.CVVResponseUnknown,
+			Response:             response.Processed.ResponseCode,
+			ErrorCode:            response.Processed.ResponseCode,
+		}, nil
+	}
+}
+
+// Capture an authorized transaction by charge ID
+func (client *CheckoutComClient) Capture(request *sleet.CaptureRequest) (*sleet.CaptureResponse, error) {
+	checkoutComClient, err := client.generateCheckoutDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	input, err := buildCaptureParams(request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := checkoutComClient.Captures(request.TransactionReference, input, nil)
+
+	if err != nil {
+		return &sleet.CaptureResponse{Success: false, ErrorCode: common.SPtr(err.Error())}, err
+	}
+
+	if response.StatusResponse.StatusCode == AcceptedStatusCode {
+		return &sleet.CaptureResponse{Success: true, TransactionReference: request.TransactionReference}, nil
+	} else {
+		return &sleet.CaptureResponse{
+			Success: false,
+			ErrorCode: common.SPtr(strconv.Itoa(response.StatusResponse.StatusCode)),
+			TransactionReference: request.TransactionReference,
+		}, nil
+	}
+}
+
+// Refund a captured transaction with amount and charge ID
+func (client *CheckoutComClient) Refund(request *sleet.RefundRequest) (*sleet.RefundResponse, error) {
+	config, err := checkout.Create(client.apiKey, nil)
+	if err != nil {
+		return nil, err
+	}
+	config.HTTPClient = client.httpClient
+
+	checkoutComClient := payments.NewClient(*config)
+
+	input, err := buildRefundParams(request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := checkoutComClient.Refunds(request.TransactionReference, input, nil)
+	if err != nil {
+		return &sleet.RefundResponse{Success: false, ErrorCode: common.SPtr(err.Error())}, err
+	}
+
+	if response.StatusResponse.StatusCode == AcceptedStatusCode {
+		return &sleet.RefundResponse{Success: true, TransactionReference: response.Accepted.Reference}, nil
+	} else {
+		return &sleet.RefundResponse{
+			Success: false,
+			ErrorCode: common.SPtr(strconv.Itoa(response.StatusResponse.StatusCode)),
+			TransactionReference: request.TransactionReference,
+		}, nil
+	}
+}
+
+// Void an authorized transaction with charge ID
+func (client *CheckoutComClient) Void(request *sleet.VoidRequest) (*sleet.VoidResponse, error) {
+	checkoutComClient, err := client.generateCheckoutDCClient()
+	if err != nil {
+		return nil, err
+	}
+
+	input, err := buildVoidParams(request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := checkoutComClient.Voids(request.TransactionReference, input, nil)
+
+	if err != nil {
+		return &sleet.VoidResponse{Success: false, ErrorCode: common.SPtr(err.Error())}, err
+	}
+
+	if response.StatusResponse.StatusCode == AcceptedStatusCode {
+		return &sleet.VoidResponse{Success: true, TransactionReference: response.Accepted.Reference}, nil
+	} else {
+		return &sleet.VoidResponse{
+			Success: false,
+			ErrorCode: common.SPtr(strconv.Itoa(response.StatusResponse.StatusCode)),
+			TransactionReference: request.TransactionReference,
+		}, nil
+	}
+}
---
+++ integration-tests/checkoutcom_test.go
@@ -0,0 +1,157 @@
+package test
+
+import (
+	"github.com/BoltApp/sleet"
+	"github.com/BoltApp/sleet/gateways/checkoutcom"
+	sleet_testing "github.com/BoltApp/sleet/testing"
+	"testing"
+	"time"
+)
+
+// TestCheckoutComAuthorizeFailed
+//
+// checkout.com has test cards here: https://www.checkout.com/docs/four/testing/response-code-testing
+// Using a rejected card number
+func TestCheckoutComAuthorizeFailed(t *testing.T) {
+	client := checkoutcom.NewClient(getEnv("CHECKOUTCOM_TEST_KEY"))
+	failedRequest := sleet_testing.BaseAuthorizationRequest()
+	failedRequest.CreditCard.Number = "4544249167673670"
+	response, err := client.Authorize(failedRequest)
+
+	if err != nil {
+		t.Errorf("Authorize request should not have an error even if authorization failed- %s", err.Error())
+	}
+
+	if response.Success {
+		t.Error("Auth response should indicate a failure")
+	}
+
+	if response.Response != "20051" {
+		t.Errorf("Response should be 20051, code for insufficient funds- %s", response.Response)
+	}
+}
+
+// TestCheckoutComAuth
+//
+// This should successfully create an authorization
+func TestCheckoutComAuth(t *testing.T) {
+	client := checkoutcom.NewClient(getEnv("CHECKOUTCOM_TEST_KEY"))
+	request := sleet_testing.BaseAuthorizationRequest()
+	auth, err := client.Authorize(request)
+	if err != nil {
+		t.Error("Authorize request should not have failed")
+	}
+
+	if !auth.Success {
+		t.Error("Resulting auth should have been successful")
+	}
+}
+
+// TestCheckoutComAuthFullCapture
+//
+// This should successfully create an authorization on checkout.com then Capture for full amount
+func TestCheckoutComAuthFullCapture(t *testing.T) {
+	client := checkoutcom.NewClient(getEnv("CHECKOUTCOM_TEST_KEY"))
+	authRequest := sleet_testing.BaseAuthorizationRequest()
+	auth, err := client.Authorize(authRequest)
+	if err != nil {
+		t.Error("Authorize request should not have failed")
+	}
+
+	if !auth.Success {
+		t.Error("Resulting auth should have been successful")
+	}
+
+	captureRequest := &sleet.CaptureRequest{
+		Amount:               &authRequest.Amount,
+		TransactionReference: auth.TransactionReference,
+		ClientTransactionReference: authRequest.ClientTransactionReference,
+		MerchantOrderReference: &authRequest.MerchantOrderReference,
+	}
+	capture, err := client.Capture(captureRequest)
+	if err != nil {
+		t.Error("Capture request should not have failed")
+	}
+
+	if !capture.Success {
+		t.Error("Resulting capture should have been successful")
+	}
+}
+
+// TestCheckoutComAuthVoid
+//
+// This should successfully create an authorization on checkout.com then Void/Cancel the Auth
+func TestCheckoutComAuthVoid(t *testing.T) {
+	client := checkoutcom.NewClient(getEnv("CHECKOUTCOM_TEST_KEY"))
+	authRequest := sleet_testing.BaseAuthorizationRequest()
+	auth, err := client.Authorize(authRequest)
+	if err != nil {
+		t.Error("Authorize request should not have failed")
+	}
+
+	if !auth.Success {
+		t.Error("Resulting auth should have been successful")
+	}
+
+	voidRequest := &sleet.VoidRequest{
+		TransactionReference: auth.TransactionReference,
+		MerchantOrderReference: &authRequest.MerchantOrderReference,
+	}
+	void, err := client.Void(voidRequest)
+	if err != nil {
+		t.Error("Void request should not have failed")
+	}
+
+	if !void.Success {
+		t.Error("Resulting void should have been successful")
+	}
+}
+
+// TestCheckoutComAuthCaptureRefund
+//
+// This should successfully create an authorization on checkout.com then Capture for full amount, then refund for full amount
+func TestCheckoutComAuthCaptureRefund(t *testing.T) {
+	client := checkoutcom.NewClient(getEnv("CHECKOUTCOM_TEST_KEY"))
+	authRequest := sleet_testing.BaseAuthorizationRequest()
+	auth, err := client.Authorize(authRequest)
+	if err != nil {
+		t.Error("Authorize request should not have failed")
+	}
+
+	if !auth.Success {
+		t.Error("Resulting auth should have been successful")
+	}
+
+	captureRequest := &sleet.CaptureRequest{
+		Amount:               &authRequest.Amount,
+		TransactionReference: auth.TransactionReference,
+		ClientTransactionReference: authRequest.ClientTransactionReference,
+		MerchantOrderReference: &authRequest.MerchantOrderReference,
+	}
+	capture, err := client.Capture(captureRequest)
+	if err != nil {
+		t.Error("Capture request should not have failed")
+	}
+
+	if !capture.Success {
+		t.Error("Resulting capture should have been successful")
+	}
+
+	time.Sleep(4 * time.Second) // Delay to make sure capture has processed
+
+	refundRequest := &sleet.RefundRequest{
+		Amount:               &authRequest.Amount,
+		TransactionReference: capture.TransactionReference,
+		ClientTransactionReference: authRequest.ClientTransactionReference,
+		MerchantOrderReference: &authRequest.MerchantOrderReference,
+	}
+
+	refund, err := client.Refund(refundRequest)
+	if err != nil {
+		t.Error("Refund request should not have failed")
+	}
+
+	if !refund.Success {
+		t.Error("Resulting refund should have been successful")
+	}
+}
