fc9e30c28c998a5d66d8a18e020793705633192e
Robert Cooper <robert.cooper@bolt.com>
2022-02-28 16:11:07 -0800

    add phonenumber + email authnet

--- testing/auth_request.go
+++ testing/auth_request.go
@@ -18,6 +18,8 @@ func BaseAuthorizationRequest() *sleet.AuthorizationRequest {
 		StreetAddress1: common.SPtr("7683 Railroad Street"),
 		Locality:       common.SPtr("Zion"),
 		RegionCode:     common.SPtr("IL"),
+		Email: 			common.SPtr("test@bolt.com"),
+		PhoneNumber:    common.SPtr("555-555-5555"),
 	}
 	card := sleet.CreditCard{
 		FirstName:       "Bolt",
