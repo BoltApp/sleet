bdc99abecc2599c42397046703066ec3f5190a49
Robert Cooper <robert.cooper@bolt.com>
2022-02-28 16:24:22 -0800

    fix apple pay test

--- gateways/authorizenet/request_builders_test.go
+++ gateways/authorizenet/request_builders_test.go
@@ -99,6 +99,10 @@ func TestBuildAuthRequest(t *testing.T) {
 							State:     base.BillingAddress.RegionCode,
 							Zip:       base.BillingAddress.PostalCode,
 							Country:   base.BillingAddress.CountryCode,
+							PhoneNumber: base.BillingAddress.PhoneNumber,
+						},
+						Customer: &Customer{
+							Email: *base.BillingAddress.Email,
 						},
 					},
 				},
