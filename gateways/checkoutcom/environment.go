d28f7c88f78a2029007734846004b80462147da7
Ashish Sharma <asharma@bolt.com>
2024-01-31 18:05:12 -0500

    Update

--- gateways/checkoutcom/environment.go
+++ gateways/checkoutcom/environment.go
@@ -1,14 +1,14 @@
 package checkoutcom
 
 import (
-	"github.com/checkout/checkout-sdk-go"
+	"github.com/checkout/checkout-sdk-go/configuration"
 
 	"github.com/BoltApp/sleet/common"
 )
 
-func GetEnv(env common.Environment) checkout.SupportedEnvironment {
+func GetEnv(env common.Environment) *configuration.CheckoutEnv {
 	if env == common.Production {
-		return checkout.Production
+		return configuration.Production()
 	}
-	return checkout.Sandbox
+	return configuration.Sandbox()
 }
